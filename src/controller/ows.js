63e9af28fbc7e7256f44c854f801255e8f78a1fd
schmidko <einfach.schmidt@gmail.com>
2025-02-08 22:12:23 +0100

    chnaged postgresql config to avoid errors

--- src/controller/ows.js
+++ src/controller/ows.js
@@ -1,40 +1,21 @@
 
 const {connectDB} = require("../db.js");
 const pg = require('pg');
-const {Client} = pg;
 
-async function getOwsData(address) {
-
-    address = await findStakeAddress(address);
-
-    if (!address) {
-        return false;
-    }
-
-    try {
-        const {db} = await connectDB();
-        const collection = db.collection("addresses");
-        const queryFind = {"stakeAddress": address};
-        const result = await collection.find(queryFind).toArray();
-        return result;
-    } catch (e) {
-        return false;
-    }
-}
+const configPG = {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: 'cexplorer',
+    port: 5432
+};
 
 async function getDrepData(addressInput) {
     const stakeAddress = await findStakeAddress(addressInput);
 
-    const config = {
-        host: process.env.PG_HOST,
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: 'cexplorer',
-        port: 5432
-    };
-
     try {
-        const client = new Client(config);
+        const {Client} = pg;
+        const client = new Client(configPG);
         await client.connect();
         query = `SELECT drep_hash.view AS drep_view from stake_address
             left join delegation_vote on delegation_vote.addr_id = stake_address.id
@@ -85,21 +66,13 @@ async function getAllAssets(addressInput) {
 async function findStakeAddress(address) {
     address = address.toLowerCase();
 
-    const config = {
-        host: process.env.PG_HOST,
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: 'cexplorer',
-        port: 5432,
-        //ssl: true
-    };
-
     if (address.indexOf('stake') === 0) {
         return address;
     }
     if (address.indexOf('addr') === 0) {
         try {
-            const client = new Client(config);
+            const {Client} = pg;
+            const client = new Client(configPG);
             await client.connect();
             query = `SELECT DISTINCT stake_address.id as stake_address_id, tx_out.address, stake_address.view as stake_address
 	        from stake_address left join tx_out on tx_out.stake_address_id = stake_address.id
